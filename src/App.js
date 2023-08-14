b4bb40f3387170a025a883fdec95e84c68530857
Bazuuko <140450044+Bazuuko@users.noreply.github.com>
2023-08-14 02:06:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -352,6 +352,40 @@ useEffect(() => {
       
       </div>
 
+<div class="card" style={{marginLeft:"70px"}} onClick={handleMintfun}>
+      <ResponsiveWrapper test>
+        <s.SpacerLarge />
+        <s.Container
+          flex={1}
+          jc={"center"}
+          ai={"center"}
+          image={CONFIG.SHOW_BACKGROUND ? "/config/images/baseButton.png" : null}
+          style={{
+            padding: 10,
+            borderRadius: 30,
+            boxShadow: "2px 10px 10px 10px rgba(0,0,0,0.2)",
+            cursor:"pointer",
+            width:"280px", 
+            height:"100px",
+
+          }}
+          
+        >
+          <s.TextTitle2
+            style={{
+              textAlign: "center",
+              letterSpacing: 3,
+              fontSize: 30,
+              color: "var(--accent-text)",
+              textShadow: "-2px -2px 0 #000, 2px -2px 0 #000, -2px 3px 0 #000, 3px 3px 0 #000",
+            }}
+          >
+            opensea
+          </s.TextTitle2>
+        </s.Container>
+
+      </ResponsiveWrapper>
+      </div>
 
       <s.SpacerLargeX />
       <s.SpacerLargeX />
